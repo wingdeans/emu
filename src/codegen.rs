fc5a9c94682e63a6d93e8fbb9cf3f7cfd11fe0e4
wingdeans <66850754+wingdeans@users.noreply.github.com>
2025-02-11 21:26:52 -0500

    Move printing into codegen

--- src/codegen.rs
+++ src/codegen.rs
@@ -96,260 +96,256 @@ fn gen_tokenfield(tokenfield: TokenField, off: u8) -> TokenStream {
     }
 }
 
-struct Codegen(TokenStream);
+// TOPLEVEL
 
-impl Codegen {
-    fn emit_subtable(&mut self, subtable: Subtable, idx: usize) {
-        let name = format_ident!("Sym{}", idx);
-        let enum_variants = subtable
-            .constructors
-            .iter()
-            .enumerate()
-            .map(|(i, constructor)| {
-                let variant_name = format_ident!("Variant{}", i);
-                let operands = constructor
-                    .operands
-                    .iter()
-                    .map(|op| format_ident!("Op{}", op));
-                quote! {
-                    #variant_name(#(#operands),*),
-                }
-            });
-
-        let decode_body = if let Some(decision) = subtable.decision {
-            gen_decision(decision, &subtable.constructors)
-        } else {
-            let decode_ops = subtable.constructors[0].operands.iter().map(|op| {
-                let op_name = format_ident!("Op{}", op);
-                quote! {
-                    #op_name::decode(&buf)?
-                }
-            });
-            quote! {
-                Some(Self::Variant0(#(#decode_ops),*))
-            }
-        };
-
-        let fmt_cases = subtable.constructors.iter().map(|constructor| {
-            let variant = format_ident!("Variant{}", constructor.id);
-            let operand_bindings =
-                (0..constructor.operands.len()).map(|i| format_ident!("op{}", i));
-
-            let operand_args = constructor.prints.iter().filter_map(|print| match print {
-                Print::Print(_) => None,
-                Print::OpPrint(idx) => Some(format_ident!("op{}", idx)),
-            });
-
-            let fstring = constructor
-                .prints
+fn gen_subtable(subtable: Subtable, idx: usize) -> TokenStream {
+    let name = format_ident!("Sym{}", idx);
+    let enum_variants = subtable
+        .constructors
+        .iter()
+        .enumerate()
+        .map(|(i, constructor)| {
+            let variant_name = format_ident!("Variant{}", i);
+            let operands = constructor
+                .operands
                 .iter()
-                .map(|print| match print {
-                    Print::Print(piece) => piece,
-                    Print::OpPrint(_) => "{}",
-                })
-                .collect::<Vec<_>>()
-                .join("");
-
+                .map(|op| format_ident!("Op{}", op));
             quote! {
-                Self::#variant(#(#operand_bindings),*) =>
-                    write!(f, #fstring, #(#operand_args),*),
+                #variant_name(#(#operands),*),
             }
         });
 
-        let pcode_cases = subtable.constructors.iter().map(|constructor| {
-            let variant = format_ident!("Variant{}", constructor.id);
-            let operand_bindings =
-                (0..constructor.operands.len()).map(|i| format_ident!("op{}", i));
-
-            let op = constructor
-                .construct_tpl
-                .iter()
-                .map(|op_tpl| format!("{:?}", op_tpl))
-                .collect::<Vec<_>>()
-                .join(", ");
-
+    let decode_body = if let Some(decision) = subtable.decision {
+        gen_decision(decision, &subtable.constructors)
+    } else {
+        let decode_ops = subtable.constructors[0].operands.iter().map(|op| {
+            let op_name = format_ident!("Op{}", op);
             quote! {
-                Self::#variant(#(#operand_bindings),*) => #op,
+                #op_name::decode(&buf)?
             }
         });
+        quote! {
+            Some(Self::Variant0(#(#decode_ops),*))
+        }
+    };
 
-        self.0.extend(quote! {
-            #[derive(Debug)]
-            enum #name {
-                #(#enum_variants)*
+    let fmt_cases = subtable.constructors.iter().map(|constructor| {
+        let variant = format_ident!("Variant{}", constructor.id);
+        let operand_bindings = (0..constructor.operands.len()).map(|i| format_ident!("op{}", i));
+
+        let operand_args = constructor.prints.iter().filter_map(|print| match print {
+            Print::Print(_) => None,
+            Print::OpPrint(idx) => Some(format_ident!("op{}", idx)),
+        });
+
+        let fstring = constructor
+            .prints
+            .iter()
+            .map(|print| match print {
+                Print::Print(piece) => piece,
+                Print::OpPrint(_) => "{}",
+            })
+            .collect::<Vec<_>>()
+            .join("");
+
+        quote! {
+            Self::#variant(#(#operand_bindings),*) =>
+                write!(f, #fstring, #(#operand_args),*),
+        }
+    });
+
+    let pcode_cases = subtable.constructors.iter().map(|constructor| {
+        let variant = format_ident!("Variant{}", constructor.id);
+        let operand_bindings = (0..constructor.operands.len()).map(|i| format_ident!("op{}", i));
+
+        let op = constructor
+            .construct_tpl
+            .iter()
+            .map(|op_tpl| format!("{:?}", op_tpl))
+            .collect::<Vec<_>>()
+            .join(", ");
+
+        quote! {
+            Self::#variant(#(#operand_bindings),*) => #op,
+        }
+    });
+
+    quote! {
+        #[derive(Debug)]
+        enum #name {
+            #(#enum_variants)*
+        }
+
+        impl #name {
+            #[allow(unused_parens)] // TODO
+            #[allow(unused_variables)]
+            fn decode(buf: &[u8]) -> Option<Self> {
+                #decode_body
             }
 
-            impl #name {
-                #[allow(unused_parens)] // TODO
-                #[allow(unused_variables)]
-                fn decode(buf: &[u8]) -> Option<Self> {
-                    #decode_body
-                }
-
-                #[allow(unused_variables)]
-                #[allow(dead_code)]
-                fn pcode(&self, vec: &mut Vec<Pcode>) {
-                    println!("    {}", match self {
-                        #(#pcode_cases)*
-                    })
-                }
-            }
-
-            impl std::fmt::Display for #name {
-                #[allow(unused_variables)]
-                fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-                    match self {
-                        #(#fmt_cases)*
-                    }
-                }
-            }
-        })
-    }
-
-    fn emit_operand(&mut self, op: Operand, idx: usize) {
-        let name = format_ident!("Op{}", idx);
-
-        let struct_body = &match op.expr {
-            OpExpr::Subsym(subsym) => Some(format_ident!("Sym{}", subsym)),
-            OpExpr::Tok(_) => Some(format_ident!("u8")),
-            _ => None,
-        };
-
-        let write = match op.expr {
-            OpExpr::Subsym(_) => quote!("{}", self.0),
-            OpExpr::Tok(_) => quote!("0x{:X}", self.0),
-            OpExpr::Unk(id) => {
-                let id = format!("UNK{}", id);
-                quote!(#id)
-            }
-        };
-
-        let decode_arg = &match op.expr {
-            OpExpr::Subsym(subsym) => {
-                let offset = op.off as usize;
-                let subsym = format_ident!("Sym{}", subsym);
-                Some(quote! { #subsym::decode(&buf[#offset..])? })
-            }
-            OpExpr::Tok(tokenfield) => Some(gen_tokenfield(tokenfield, op.off)),
-            _ => None,
-        };
-
-        self.0.extend(quote! {
-            #[derive(Debug)]
-            struct #name(#struct_body);
-
-            impl #name {
-                #[allow(unused_variables)]
-                fn decode(buf: &[u8]) -> Option<Self> {
-                    Some(Self(#decode_arg))
-                }
-            }
-
-            impl std::fmt::Display for #name {
-                fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-                    write!(f, #write)
-                }
-            }
-        })
-    }
-
-    fn emit_varnode(&mut self, text: &str, idx: usize) {
-        let name = format_ident!("Sym{}", idx);
-
-        self.0.extend(quote! {
-            #[derive(Debug)]
+            #[allow(unused_variables)]
             #[allow(dead_code)]
-            struct #name();
-
-            #[allow(dead_code)]
-            impl #name {
-                fn decode(_: &[u8]) -> Option<Self> {
-                    Some(Self())
-                }
-            }
-
-            impl std::fmt::Display for #name {
-                fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-                    write!(f, #text)
-                }
-            }
-        })
-    }
-
-    fn emit_varlist(&mut self, varlist: Varlist, idx: usize) {
-        let name = format_ident!("Sym{}", idx);
-        let enum_body = varlist
-            .vars
-            .iter()
-            .enumerate()
-            .filter_map(|(i, maybe_idx)| {
-                let variant_name = format_ident!("Variant{}", i);
-                maybe_idx.map(|idx| {
-                    let variant_body = format_ident!("Sym{}", idx);
-                    Some(quote! {
-                        #variant_name(#variant_body),
-                    })
+            fn pcode(&self, vec: &mut Vec<Pcode>) {
+                println!("    {}", match self {
+                    #(#pcode_cases)*
                 })
-            });
-
-        let decode_arms = varlist
-            .vars
-            .iter()
-            .enumerate()
-            .filter_map(|(i, maybe_idx)| {
-                maybe_idx.map(|idx| {
-                    let variant_name = format_ident!("Variant{}", i);
-                    let sym_name = format_ident!("Sym{}", idx);
-                    let pat = i as u8;
-                    Some(quote! {
-                        #pat => Some(Self::#variant_name(#sym_name::decode(buf)?)),
-                    })
-                })
-            });
-
-        let write_arms = varlist
-            .vars
-            .iter()
-            .enumerate()
-            .filter_map(|(i, maybe_idx)| {
-                maybe_idx.map(|_| {
-                    let variant_name = format_ident!("Variant{}", i);
-                    Some(quote! {
-                        Self::#variant_name(x) => x.fmt(f),
-                    })
-                })
-            });
-
-        let tokenfield = gen_tokenfield(varlist.tokenfield, 0);
-
-        self.0.extend(quote! {
-            #[derive(Debug)]
-            enum #name {
-                #(#enum_body)*
             }
+        }
 
-            impl #name {
-                fn decode(buf: &[u8]) -> Option<Self> {
-                    match #tokenfield {
-                        #(#decode_arms)*
-                        _ => None
-                    }
+        impl std::fmt::Display for #name {
+            #[allow(unused_variables)]
+            fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+                match self {
+                    #(#fmt_cases)*
                 }
             }
-
-            impl std::fmt::Display for #name {
-                fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-                    match self {
-                        #(#write_arms)*
-                    }
-                }
-            }
-        })
+        }
     }
 }
 
-pub(crate) fn emit(sleigh: SymbolTable) -> TokenStream {
-    let mut tokens = quote! {
+fn gen_operand(op: Operand, idx: usize) -> TokenStream {
+    let name = format_ident!("Op{}", idx);
+
+    let struct_body = &match op.expr {
+        OpExpr::Subsym(subsym) => Some(format_ident!("Sym{}", subsym)),
+        OpExpr::Tok(_) => Some(format_ident!("u8")),
+        _ => None,
+    };
+
+    let write = match op.expr {
+        OpExpr::Subsym(_) => quote!("{}", self.0),
+        OpExpr::Tok(_) => quote!("0x{:X}", self.0),
+        OpExpr::Unk(id) => {
+            let id = format!("UNK{}", id);
+            quote!(#id)
+        }
+    };
+
+    let decode_arg = &match op.expr {
+        OpExpr::Subsym(subsym) => {
+            let offset = op.off as usize;
+            let subsym = format_ident!("Sym{}", subsym);
+            Some(quote! { #subsym::decode(&buf[#offset..])? })
+        }
+        OpExpr::Tok(tokenfield) => Some(gen_tokenfield(tokenfield, op.off)),
+        _ => None,
+    };
+
+    quote! {
+        #[derive(Debug)]
+        struct #name(#struct_body);
+
+        impl #name {
+            #[allow(unused_variables)]
+            fn decode(buf: &[u8]) -> Option<Self> {
+                Some(Self(#decode_arg))
+            }
+        }
+
+        impl std::fmt::Display for #name {
+            fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+                write!(f, #write)
+            }
+        }
+    }
+}
+
+fn gen_varnode(text: &str, idx: usize) -> TokenStream {
+    let name = format_ident!("Sym{}", idx);
+
+    quote! {
+        #[derive(Debug)]
+        #[allow(dead_code)]
+        struct #name();
+
+        #[allow(dead_code)]
+        impl #name {
+            fn decode(_: &[u8]) -> Option<Self> {
+                Some(Self())
+            }
+        }
+
+        impl std::fmt::Display for #name {
+            fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+                write!(f, #text)
+            }
+        }
+    }
+}
+
+fn gen_varlist(varlist: Varlist, idx: usize) -> TokenStream {
+    let name = format_ident!("Sym{}", idx);
+    let enum_body = varlist
+        .vars
+        .iter()
+        .enumerate()
+        .filter_map(|(i, maybe_idx)| {
+            let variant_name = format_ident!("Variant{}", i);
+            maybe_idx.map(|idx| {
+                let variant_body = format_ident!("Sym{}", idx);
+                Some(quote! {
+                    #variant_name(#variant_body),
+                })
+            })
+        });
+
+    let decode_arms = varlist
+        .vars
+        .iter()
+        .enumerate()
+        .filter_map(|(i, maybe_idx)| {
+            maybe_idx.map(|idx| {
+                let variant_name = format_ident!("Variant{}", i);
+                let sym_name = format_ident!("Sym{}", idx);
+                let pat = i as u8;
+                Some(quote! {
+                    #pat => Some(Self::#variant_name(#sym_name::decode(buf)?)),
+                })
+            })
+        });
+
+    let write_arms = varlist
+        .vars
+        .iter()
+        .enumerate()
+        .filter_map(|(i, maybe_idx)| {
+            maybe_idx.map(|_| {
+                let variant_name = format_ident!("Variant{}", i);
+                Some(quote! {
+                    Self::#variant_name(x) => x.fmt(f),
+                })
+            })
+        });
+
+    let tokenfield = gen_tokenfield(varlist.tokenfield, 0);
+
+    quote! {
+        #[derive(Debug)]
+        enum #name {
+            #(#enum_body)*
+        }
+
+        impl #name {
+            fn decode(buf: &[u8]) -> Option<Self> {
+                match #tokenfield {
+                    #(#decode_arms)*
+                    _ => None
+                }
+            }
+        }
+
+        impl std::fmt::Display for #name {
+            fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+                match self {
+                    #(#write_arms)*
+                }
+            }
+        }
+    }
+}
+
+pub(crate) fn emit(sleigh: SymbolTable) -> Result<(), Box<dyn std::error::Error>> {
+    let tokens = quote! {
         #[derive(Debug)]
         pub(crate) struct Pcode();
 
@@ -372,18 +368,18 @@ pub(crate) fn emit(sleigh: SymbolTable) -> TokenStream {
             }
         }
     };
-
-    let mut cg = Codegen(tokens);
+    println!("{}", prettyplease::unparse(&syn::parse2(tokens)?));
 
     for (i, sym) in sleigh.syms.into_iter().enumerate() {
-        match sym {
-            Sym::Subtable(subtable) => cg.emit_subtable(subtable, i),
-            Sym::Op(operand) => cg.emit_operand(operand, i),
-            Sym::Varnode => cg.emit_varnode(&sleigh.sym_names[i], i),
-            Sym::Varlist(varlist) => cg.emit_varlist(varlist, i),
-            _ => (),
-        }
+        let tokens = match sym {
+            Sym::Subtable(subtable) => gen_subtable(subtable, i),
+            Sym::Op(operand) => gen_operand(operand, i),
+            Sym::Varnode => gen_varnode(&sleigh.sym_names[i], i),
+            Sym::Varlist(varlist) => gen_varlist(varlist, i),
+            _ => continue,
+        };
+        println!("{}", prettyplease::unparse(&syn::parse2(tokens)?));
     }
 
-    cg.0
+    Ok(())
 }
